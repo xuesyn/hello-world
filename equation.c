c17a896534c4bc2d68718f72383857674dbaf7d2
Your Name <you@example.com>
2018-10-27 23:24:16 +0800

    first homework

--- equation.c
+++ equation.c
@@ -1,4 +1,14 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include<stdio.h>
+int main()
+{
+    long a,b,c,d,e,f;
+    double x,y;
+    printf("please input a,b,c,d,e,f as follow:\n"); 
+    scanf("%ld%ld%ld%ld%ld%ld",&a,&b,&c,&d,&e,&f);
+    y=(double)((double)(c*d-a*f)/(double)(d*b-a*e));
+    x=(double)((double)(c*e-b*f)/(double)(e*a-b*d));
+    printf("x=%lf\n",x);
+    printf("y=%lf",y);
+    return 0;
+}
+
--- equation1.c
+++ equation1.c
@@ -1,3 +1,21 @@
-//求解一元二次方程的根
-//查阅sqrt()函数
-//注意有没有解
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    long a,b,c;
+    double x1,x2;
+    printf("the equality exist as ax^2+bx+c=0 then please input a,b,c as follow:\n");
+    scanf("%ld%ld%ld",&a,&b,&c);
+    if (b*b<4*a*c)
+    printf("there does not exist any root.");
+    else
+    { x1=(double)(((double)(-1)*b+sqrt((double)(b*b-4*a*c)))/(double)(2*a));
+      x2=(double)(((double)(-1)*b-sqrt((double)(b*b-4*a*c)))/(double)(2*a));
+    }
+    if (b*b==4*a*c)
+    printf("x1=x2=%lf",x1);
+    if (b*b>4*a*c)
+    printf("x1=%lf\nx2=%lf",x1,x2);
+    return 0;
+}
+
