6ffe1ee75c39524375f97a0569f6df2b0a9dc16c
Your Name <you@example.com>
2018-10-31 20:05:10 +0800

    qwq

--- equation.c
+++ equation.c
@@ -1,14 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    long a,b,c,d,e,f;
+    long a,b,c,d,e,f,q;
     double x,y;
     printf("please input a,b,c,d,e,f as follow:\n"); 
     scanf("%ld%ld%ld%ld%ld%ld",&a,&b,&c,&d,&e,&f);
+    q=0;
+    if (d*b-a*e!=0) 
+    {
     y=(double)((double)(c*d-a*f)/(double)(d*b-a*e));
     x=(double)((double)(c*e-b*f)/(double)(e*a-b*d));
-    printf("x=%lf\n",x);
+}
+    else
+    {q=1;
+    if ((double)f-c*((double)d/(double)a)==0)
+    printf("there are uncountable roots");
+    else 
+    printf("there is no root");
+}
+if (q==0)
+{
+
+	printf("x=%lf\n",x);
     printf("y=%lf",y);
     return 0;
+}
 }
 
